dd98d670a664d7376990ac0270035c56344f7b44
ipyth <mylhmail@163.com>
2018-04-10 16:30:01 +0800

    simple restful api with flask-restful

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return "Hello World!"
+api = Api(app)
 
 
-@app.route('/users', methods=["GET"])
-def get_user_lists():
-    return "user lists"
+class UserApi(Resource):
+    def get(self, user_id=None):
+        if user_id:
+            return "user_id: " + user_id
+        else:
+            return "user lists"
+
+    def post(self):
+        return "create user"
 
 
-@app.route('/users', methods=["POST"])
-def create_user():
-    return "create user"
+class Index(Resource):
+    def get(self):
+        return "Hello World!"
 
 
-@app.route('/users/<user_id>', methods=["GET"])
-def get_user(user_id):
-    return "user_id: " + user_id
-
+api.add_resource(Index, '/')
+api.add_resource(UserApi, '/users/', '/users/<user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
