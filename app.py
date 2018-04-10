c2264cf6d8f89ec84456dbc68bd2cf1a76320124
ipyth <mylhmail@163.com>
2018-04-10 16:06:28 +0800

    simple restful api without flask-restful

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ def index():
     return "Hello World!"
 
 
+@app.route('/users', methods=["GET"])
+def get_user_lists():
+    return "user lists"
+
+
+@app.route('/users', methods=["POST"])
+def create_user():
+    return "create user"
+
+
+@app.route('/users/<user_id>', methods=["GET"])
+def get_user(user_id):
+    return "user_id: " + user_id
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
