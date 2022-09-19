021e811c571b2bfdff6a515eca3a7ac7e5dfeb16
luohu1 <mylhmail@163.com>
2022-09-19 16:07:24 +0800

    feat: OpenAPI schema docs.

--- apiserver/core/settings/local.py
+++ apiserver/core/settings/local.py
@@ -5,4 +5,16 @@ DEBUG = True
 
 INSTALLED_APPS += [
     'rest_framework',
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'DRF APIServer',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    # 'SERVE_INCLUDE_SCHEMA': False,
+}
--- apiserver/core/urls.py
+++ apiserver/core/urls.py
@@ -1,21 +1,10 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular import views as schema_view
 
 urlpatterns = [
+    path('api/schema/', schema_view.SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', schema_view.SpectacularSwaggerView.as_view(), name='swagger-ui'),
+    path('api/schema/redoc/', schema_view.SpectacularRedocView.as_view(), name='redoc'),
     path('admin/', admin.site.urls),
 ]
